c6753ef5925e49776a881d62228d3f58e5df1791
juliendec <juliendecoen2308@gmail.com>
2023-09-15 13:47:47 +0200

    1.1-1.2 : import image

--- exercices/1.1-1.2/1.2/src/components/Header/Header.jsx
+++ exercices/1.1-1.2/1.2/src/components/Header/Header.jsx
@@ -1,6 +1,9 @@
+import logo from './../../assets/LOGO_HEVINCI.png';
+
 const Header = (props) => (
     <div>
         <h1>{props.course}</h1>
+        <img src={logo} alt="img ecole"></img>
     </div>
   )
 
