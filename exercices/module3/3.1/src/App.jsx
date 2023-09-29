306dfe5b4f52d5f3a957122728a4057001447c0d
juliendec <juliendecoen2308@gmail.com>
2023-09-29 12:14:08 +0200

    module3 : basics

---
+++ exercices/module3/3.1/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [counter, setCounter] = useState(0)
+
+
+
+  return (
+    <div>
+      <h1>Test</h1>
+    </div>
+  )
+} 
+
+  export default App 
